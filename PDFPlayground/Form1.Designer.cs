02daabbd87595413183e975b38a3db7d8cf347c9
DESKTOP-E2AS038\heliodoros <hpanagiotopoulos@cognity.gr>
2024-05-12 22:02:50 +0300

    Add project files.

---
+++ PDFPlayground/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace PDFPlayground
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            button3 = new Button();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            progressBar1 = new ProgressBar();
+            statusLabel = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Select PDF";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ControlLightLight;
+            button2.Location = new Point(12, 45);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Select Output";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(112, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(112, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 15);
+            label2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.PaleGreen;
+            button3.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 64);
+            button3.FlatAppearance.BorderSize = 10;
+            button3.Location = new Point(3, 132);
+            button3.Name = "button3";
+            button3.Size = new Size(118, 23);
+            button3.TabIndex = 5;
+            button3.Text = "Compress and Split";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(156, 135);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(498, 10);
+            progressBar1.TabIndex = 6;
+            // 
+            // statusLabel
+            // 
+            statusLabel.AutoSize = true;
+            statusLabel.BackColor = SystemColors.ControlLight;
+            statusLabel.ForeColor = SystemColors.GrayText;
+            statusLabel.Location = new Point(326, 117);
+            statusLabel.Name = "statusLabel";
+            statusLabel.Size = new Size(0, 15);
+            statusLabel.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ControlLight;
+            label4.ForeColor = SystemColors.GrayText;
+            label4.Location = new Point(440, 117);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 15);
+            label4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(707, 166);
+            Controls.Add(label4);
+            Controls.Add(statusLabel);
+            Controls.Add(progressBar1);
+            Controls.Add(button3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            SizeGripStyle = SizeGripStyle.Hide;
+            Text = "Split and Compress PDF";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Button button3;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private ProgressBar progressBar1;
+        private Label statusLabel;
+        private Label label4;
+    }
+}
---
+++ PDFPlayground/Form1.cs
@@ -0,0 +1,153 @@
+using Aspose.Pdf;
+using Aspose.Pdf.Optimization;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFPlayground
+{
+    public partial class Form1 : Form
+    {
+        private string fileName;
+        private string outputDirectory;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Aspose.Pdf.License license1 = new Aspose.Pdf.License();
+            license1.SetLicense("C:\\Project_Horizon\\Files\\Application\\Aspose\\Aspose.Total.NET.lic");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.AddExtension = true;
+            openFileDialog1.DefaultExt = "pdf";
+            openFileDialog1.Filter = "PDF files (*.pdf)|*.pdf|All files (*.*)|*.*";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                label1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                outputDirectory = folderBrowserDialog1.SelectedPath;
+                label2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(outputDirectory))
+            {
+                MessageBox.Show("Please select both input PDF file and output directory.");
+                return;
+            }
+
+            //DialogResult dialogResult = MessageBox.Show("Begin operation?", "Confirmation", MessageBoxButtons.YesNo);
+            //if (dialogResult != DialogResult.Yes)
+            //    return;
+
+            try
+            {
+                progressBar1.ForeColor = System.Drawing.Color.BlanchedAlmond;
+                progressBar1.Style = ProgressBarStyle.Continuous;
+                
+                Document pdfDocument = new Document(fileName);
+                double maxSizeInKB = 14500;
+                int totalPages = pdfDocument.Pages.Count;
+                Debug.WriteLine(fileName);
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = totalPages;
+                progressBar1.Value = totalPages;
+                statusLabel.Text = "Compressing...";
+                label4.Text = $"Total pages: {totalPages}";
+                await Task.Run(async () =>
+                {
+                    var optimizeOptions = new Aspose.Pdf.Optimization.OptimizationOptions();
+                    optimizeOptions.LinkDuplcateStreams = true;
+                    optimizeOptions.RemoveUnusedObjects = true;
+                    //optimizeOptions.RemoveUnusedStreams = true; <- performance drop, no benefit
+                    optimizeOptions.AllowReusePageContent = true;
+                    optimizeOptions.UnembedFonts = true;
+                    //optimizeOptions.SubsetFonts = true; <- extreme performance drop, no benefit
+                    //optimizeOptions.RemovePrivateInfo = true;
+                    //optimizeOptions.ImageCompressionOptions.CompressImages = true;
+                    //optimizeOptions.ImageCompressionOptions.ImageQuality = 50;
+                    //optimizeOptions.ImageCompressionOptions.ResizeImages = true;
+                    //optimizeOptions.ImageCompressionOptions.MaxResolution = 100;
+                    Console.WriteLine(pdfDocument.FileName);
+                    pdfDocument.OptimizeResources(optimizeOptions);
+                    //pdfDocument.Save(Path.Combine(outputDirectory, $"compressed_output.pdf"));
+                    GC.Collect();
+
+                });
+
+
+                await Task.Run(() =>
+                {
+                    int splitIndex = 1;
+                    int pageCount = 0;
+                    Document currentPdfDocument = new Document();
+                    string directoryPath = $"{outputDirectory}\\{Path.GetFileNameWithoutExtension(fileName)}";
+
+                    // Check if the directory doesn't exist
+                    if (!Directory.Exists(directoryPath))
+                    {
+                        // Create the directory
+                        Directory.CreateDirectory(directoryPath);
+                    }
+
+                    foreach (Page pdfPage in pdfDocument.Pages)
+                    {
+                        currentPdfDocument.Pages.Add(pdfPage);
+
+                        pageCount++;
+                        if (pageCount % 100 == 0 || pageCount == totalPages)
+                        {
+                            progressBar1.Invoke((Action)(() => progressBar1.Value = pageCount));
+                            statusLabel.Invoke((Action)(() => statusLabel.Text = pageCount.ToString()));
+
+                            // Calculate the size of the current document
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                currentPdfDocument.Save(ms);
+                                long currentSizeInBytes = ms.Length;
+
+                                long currentSizeInKB = currentSizeInBytes / 1000;
+
+                                if (currentSizeInKB > maxSizeInKB || pageCount == totalPages)
+                                {
+                                    string outputPath = Path.Combine(directoryPath, $"{splitIndex}_{Path.GetFileName(fileName)}");
+                                    currentPdfDocument.Save(outputPath);
+                                    splitIndex++;
+                                    currentPdfDocument = new Document();
+                                }
+                            }
+                        }
+                    }
+                });
+                //progressBar1.BackColor = System.Drawing.Color.Blue;
+                //progressBar1.ForeColor = System.Drawing.Color.BlanchedAlmond;
+                statusLabel.Text = "Completed";
+                //play windows info sound
+                System.Media.SystemSounds.Asterisk.Play();
+                //MessageBox.Show("PDF splitting completed successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                progressBar1.BackColor = System.Drawing.Color.Red;
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
