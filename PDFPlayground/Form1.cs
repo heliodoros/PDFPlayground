6e12b0b651794beebf9c4e63dd881012404051fb
DESKTOP-E2AS038\heliodoros <hpanagiotopoulos@cognity.gr>
2024-05-22 14:34:33 +0300

    z

--- PDFPlayground/Form1.cs
+++ PDFPlayground/Form1.cs
@@ -71,71 +71,63 @@ namespace PDFPlayground
                 progressBar1.Value = totalPages;
                 statusLabel.Text = "Compressing...";
                 label4.Text = $"Total pages: {totalPages}";
-                await Task.Run(async () =>
+
+                var optimizeOptions = new Aspose.Pdf.Optimization.OptimizationOptions();
+                optimizeOptions.LinkDuplcateStreams = true;
+                optimizeOptions.RemoveUnusedObjects = true;
+                //optimizeOptions.RemoveUnusedStreams = true; <- performance drop, no benefit
+                optimizeOptions.AllowReusePageContent = true;
+                optimizeOptions.UnembedFonts = true;
+                //optimizeOptions.SubsetFonts = true; <- extreme performance drop, no benefit
+                //optimizeOptions.RemovePrivateInfo = true;
+                //optimizeOptions.ImageCompressionOptions.CompressImages = true;
+                //optimizeOptions.ImageCompressionOptions.ImageQuality = 50;
+                //optimizeOptions.ImageCompressionOptions.ResizeImages = true;
+                //optimizeOptions.ImageCompressionOptions.MaxResolution = 100;
+                Console.WriteLine(pdfDocument.FileName);
+                pdfDocument.OptimizeResources(optimizeOptions);
+                //pdfDocument.Save(Path.Combine(outputDirectory, $"compressed_output.pdf"));
+                GC.Collect();
+                int splitIndex = 1;
+                int pageCount = 0;
+                Document currentPdfDocument = new Document();
+                string directoryPath = $"{outputDirectory}\\{Path.GetFileNameWithoutExtension(fileName)}";
+
+                // Check if the directory doesn't exist
+                if (!Directory.Exists(directoryPath))
                 {
-                    var optimizeOptions = new Aspose.Pdf.Optimization.OptimizationOptions();
-                    optimizeOptions.LinkDuplcateStreams = true;
-                    optimizeOptions.RemoveUnusedObjects = true;
-                    //optimizeOptions.RemoveUnusedStreams = true; <- performance drop, no benefit
-                    optimizeOptions.AllowReusePageContent = true;
-                    optimizeOptions.UnembedFonts = true;
-                    //optimizeOptions.SubsetFonts = true; <- extreme performance drop, no benefit
-                    //optimizeOptions.RemovePrivateInfo = true;
-                    //optimizeOptions.ImageCompressionOptions.CompressImages = true;
-                    //optimizeOptions.ImageCompressionOptions.ImageQuality = 50;
-                    //optimizeOptions.ImageCompressionOptions.ResizeImages = true;
-                    //optimizeOptions.ImageCompressionOptions.MaxResolution = 100;
-                    Console.WriteLine(pdfDocument.FileName);
-                    pdfDocument.OptimizeResources(optimizeOptions);
-                    //pdfDocument.Save(Path.Combine(outputDirectory, $"compressed_output.pdf"));
-                    GC.Collect();
+                    // Create the directory
+                    Directory.CreateDirectory(directoryPath);
+                }
 
-                });
-
-
-                await Task.Run(() =>
+                foreach (Page pdfPage in pdfDocument.Pages)
                 {
-                    int splitIndex = 1;
-                    int pageCount = 0;
-                    Document currentPdfDocument = new Document();
-                    string directoryPath = $"{outputDirectory}\\{Path.GetFileNameWithoutExtension(fileName)}";
+                    currentPdfDocument.Pages.Add(pdfPage);
 
-                    // Check if the directory doesn't exist
-                    if (!Directory.Exists(directoryPath))
+                    pageCount++;
+                    if (pageCount % 100 == 0 || pageCount == totalPages)
                     {
-                        // Create the directory
-                        Directory.CreateDirectory(directoryPath);
-                    }
+                        progressBar1.Invoke((Action)(() => progressBar1.Value = pageCount));
+                        statusLabel.Invoke((Action)(() => statusLabel.Text = pageCount.ToString()));
 
-                    foreach (Page pdfPage in pdfDocument.Pages)
-                    {
-                        currentPdfDocument.Pages.Add(pdfPage);
-
-                        pageCount++;
-                        if (pageCount % 100 == 0 || pageCount == totalPages)
+                        // Calculate the size of the current document
+                        using (MemoryStream ms = new MemoryStream())
                         {
-                            progressBar1.Invoke((Action)(() => progressBar1.Value = pageCount));
-                            statusLabel.Invoke((Action)(() => statusLabel.Text = pageCount.ToString()));
+                            currentPdfDocument.Save(ms);
+                            long currentSizeInBytes = ms.Length;
 
-                            // Calculate the size of the current document
-                            using (MemoryStream ms = new MemoryStream())
+                            long currentSizeInKB = currentSizeInBytes / 1000;
+
+                            if (currentSizeInKB > maxSizeInKB || pageCount == totalPages)
                             {
-                                currentPdfDocument.Save(ms);
-                                long currentSizeInBytes = ms.Length;
-
-                                long currentSizeInKB = currentSizeInBytes / 1000;
-
-                                if (currentSizeInKB > maxSizeInKB || pageCount == totalPages)
-                                {
-                                    string outputPath = Path.Combine(directoryPath, $"{splitIndex}_{Path.GetFileName(fileName)}");
-                                    currentPdfDocument.Save(outputPath);
-                                    splitIndex++;
-                                    currentPdfDocument = new Document();
-                                }
+                                string outputPath = Path.Combine(directoryPath, $"{splitIndex}_{Path.GetFileName(fileName)}");
+                                currentPdfDocument.Save(outputPath);
+                                splitIndex++;
+                                currentPdfDocument = new Document();
                             }
                         }
                     }
-                });
+                }
                 //progressBar1.BackColor = System.Drawing.Color.Blue;
                 //progressBar1.ForeColor = System.Drawing.Color.BlanchedAlmond;
                 statusLabel.Text = "Completed";
